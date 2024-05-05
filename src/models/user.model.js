7d5da087e1be74cd37e34284b36fab8a4ed5d6fa
Moderantem <moderantem@gmail.com>
2024-05-04 20:51:00 -0600

    enums

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, index: true, required: true, unique: true },
-  name: String,
-  last_name: String,
-  password: String,
+  name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 const userModel = mongoose.model("User", userSchema);
